39fd4054811b63d4a2f4aad3d66441052d9e1125
bmangeat <brice.mangeat@gmail.com>
2021-09-20 10:50:40 +0200

    :iphone: Css content

--- src/pages/Login.page.vue
+++ src/pages/Login.page.vue
@@ -2,19 +2,29 @@
 .rounded {
   border-radius: 15px;
 }
+
 .btn-submit {
   font-size: 20px;
 }
 
 .main-connect {
   align-content: space-around;
+  max-width: 500px;
+}
+
+.btn-forget-pwd {
+  text-transform: unset;
+}
+
+.btn-forget-pwd:before {
+  box-shadow: unset;
 }
 </style>
 <template>
-  <q-page class="main-connect padding window-height window-width row justify-center items-center q-pa-md">
+  <q-page class="main-connect padding window-height q-mx-auto row justify-center items-center q-pa-md">
     <img src="../assets/images/logo.png" alt="" width="152">
-    <q-card class="row justify-center q-py-xl q-px-md full-width rounded">
-      <q-form @submit="onSubmit" class="full-width q-px-md">
+    <q-form @submit="onSubmit" class="full-width">
+      <q-card class="justify-center q-py-xl q-px-xl full-width rounded">
         <div class="row content-center">
           <img src="../assets/images/icon-user.svg" alt="">
           <label class="text-uppercase text-bold q-pl-sm">Ton blaz</label>
@@ -36,15 +46,18 @@
           type="password"
           placeholder="Mot de passe"
         />
-      </q-form>
-    </q-card>
-    <div class="full-width ">
+      </q-card>
       <q-btn
         class="q-ma-xs full-width q-py-md rounded btn-submit text-bold"
         label="Se connecter"
         type="submit"
-        color="primary"/>
-      <p class="text-center q-mt-md">Mot de passe oublié ?</p>
+        color="primary"
+        @click="onSubmit"/>
+
+      <q-btn :to="{ name:'login' }" class="btn-forget-pwd text-center q-mt-md full-width ">Mot de passe oublié ?</q-btn>
+    </q-form>
+    <div class="full-width ">
+
     </div>
   </q-page>
 </template>
